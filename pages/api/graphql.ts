352200eeae02ecb848ff4dce8d56fb4fefd85ae9
Kryštof <krystof.kratky2003@gmail.com>
2021-06-24 13:12:06 +0200

    graphql get user info

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -7,6 +7,7 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { join } from 'path';
 import neo4j from 'neo4j-driver';
+import DbConnector from '../../src/database/driver';
 
 const loadedFiles = loadFilesSync(join(process.cwd(), '**/*.graphqls'));
 const typeDefs = mergeTypeDefs(loadedFiles);
@@ -17,41 +18,33 @@ const resolvers = {
     hello: (_parent, _args, _context) => {
       return 'Hello';
     },
+    // @ts-ignore
+    getUserInfo: async (_parent, { input }, _context) => {
+      const query = `MATCH (n:User) WHERE n.username = "${input.username}" RETURN n`;
+
+      const driver = DbConnector();
+      const session = driver.session();
+
+      const result = await session.run(query);
+
+      driver.close();
+
+      return result.records[0] === undefined
+        ? null
+        : result.records[0].get('n').properties;
+    },
   },
   Mutation: {
     // @ts-ignore
-    createUser: (_parent, { input }, _context) => {
-      const driver = neo4j.driver(
-        process.env.NEO4J_HOST || 'bolt://localhost:7687',
-        neo4j.auth.basic(
-          process.env.NEO4J_USER || 'neo4j',
-          process.env.NEO4J_PASSWORD || 'password'
-        )
-      );
+    createUser: async (_parent, { input }, _context) => {
+      const query = `MATCH (n:User) WHERE n.username = "${input.username}" RETURN n`;
 
+      const driver = DbConnector();
       const session = driver.session();
 
-      session
-        .run(
-          `Create (n:user {username : "${input.username}", first_name:"${
-            input.first_name
-          }",last_name:"${input.last_name}", email:"${
-            input.email
-          }", password:"${
-            input.password
-          }", authenticated:"false", created_at:"${new Date().getTime()}"} )`
-        )
-        .then(function (result) {
-          result.records.forEach(function (record) {
-            console.log(record.get('name'));
-          });
-          session.close();
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-
+      await session.run(query);
       driver.close();
+
       return 'done';
     },
   },
---
+++ src/database/driver.ts
@@ -0,0 +1,13 @@
+import neo4j from 'neo4j-driver';
+
+const Driver = () => {
+  const driver = neo4j.driver(
+    process.env.NEO4J_HOST || 'bolt://localhost:7687',
+    neo4j.auth.basic(
+      process.env.NEO4J_USER || 'neo4j',
+      process.env.NEO4J_PASSWORD || 'password'
+    )
+  );
+  return driver;
+};
+export default Driver;
