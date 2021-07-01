803f6301bea39edbf7ab3f3ae2813256ff44733d
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-07-01 20:38:51 +0200

    mapbox

--- src/components/mainPage/map/map.tsx
+++ src/components/mainPage/map/map.tsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, {
+  useEffect,
+  Component,
+  useRef,
+  useMemo,
+  useCallback,
+} from 'react';
+import { BsGeoAlt } from 'react-icons/bs';
+
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
   ZoomControl,
+  useMap,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useState } from 'react';
 
 const Map = () => {
-  return (
-    <MapContainer
-      center={[50, 14]}
-      zoom={15}
-      minZoom={3}
-      style={{ height: '100%', width: '100%' }}
-      zoomControl={false}
-    >
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+  const [coordinates, setCoordinates] = useState([50, 15]);
 
-      <TileLayer
-        // @ts-ignore
-        attribution="<a href='https://github.com/krystofex/hiStories'>hiStories</a>"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <ZoomControl position="bottomright" />
-    </MapContainer>
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setCoordinates([position.coords.latitude, position.coords.longitude]);
+    });
+  }, []);
+
+  return (
+    <>
+      <MapContainer
+        center={coordinates}
+        zoom={15}
+        minZoom={3}
+        style={{ height: '100%', width: '100%' }}
+        zoomControl={false}
+        scrollWheelZoom={true}
+        className="z-0"
+      >
+        <TileLayer
+          // @ts-ignore
+          attribution="<a href='https://github.com/krystofex/hiStories'>hiStories</a>"
+          url={`https://api.mapbox.com/styles/v1/${process.env.NEXT_PUBLIC_MAPBOX_USER}/${process.env.NEXT_PUBLIC_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
+        />
+        <ZoomControl position="bottomright" />
+      </MapContainer>
+      <button className="absolute z-50 top-10 right-10">
+        <BsGeoAlt />
+      </button>
+    </>
   );
 };
 
