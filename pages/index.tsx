db81a2f6a02675d717aabe5627d2c2eca0db2f7d
Kryštof <krystof.kratky2003@gmail.com>
2021-07-08 19:15:17 +0200

    create post page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosArrowBack, IoIosSettings } from 'react-icons/io';
 import { FiPlusCircle } from 'react-icons/fi';
 import { CgProfile } from 'react-icons/cg';
 
-import { Post, Map, Search } from '../src/components/mainPage';
+import { Post, Map, Search, CreatePost } from '../src/components/mainPage';
 
 const Home = () => {
+  const [page, setPage] = useState('feed');
+
   return (
     <>
       <Head>
@@ -21,43 +23,51 @@ const Home = () => {
       <div className="flex w-screen">
         <div className="p-4 left-0 top-0 w-1/2 max-w-sidebar"></div>
         <div className="absolute p-4 left-0 top-0 w-1/2 max-w-sidebar h-screen">
-          <div className="bg-white bg-opacity-80 w-full h-full rounded-lg p-2 ">
-            <div className=" overflow-y-scroll scrollbar-hide overflow-x-hidden h-content">
-              <Post
-                username="czM1K3"
-                url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
-              />
-              <Post
-                username="kahy9"
-                url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
-              />
-              <Post
-                username="krystofex"
-                url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
-              />
-              <Post
-                username="kewin"
-                url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
-              />
-            </div>
+          <div className="bg-white bg-opacity-75 w-full h-full rounded-lg p-2 backdrop-filter backdrop-blur-md">
+            {page === 'feed' ? (
+              <div className=" overflow-y-scroll scrollbar-hide overflow-x-hidden h-content">
+                <Post
+                  username="czM1K3"
+                  url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
+                />
+                <Post
+                  username="kahy9"
+                  url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
+                />
+                <Post
+                  username="krystofex"
+                  url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
+                />
+                <Post
+                  username="kewin"
+                  url="https://images.unsplash.com/photo-1561457013-a8b23513739a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1124&q=80"
+                />
+              </div>
+            ) : (
+              <>
+                <a onClick={() => setPage('feed')}>{'<back'}</a>
+                <CreatePost />
+              </>
+            )}
           </div>
         </div>
         <div className="z-10 h-10 mt-4">
           <IoIosArrowBack
-            className="bg-opacity-80 bg-white rounded-lg text-black h-10 w-10 p-2 mb-2"
+            className="bg-opacity-75 backdrop-filter backdrop-blur-md bg-white rounded-lg text-black h-10 w-10 p-2 mb-2"
             size={8}
           />
           <CgProfile
-            className="bg-opacity-80 bg-white rounded-lg text-black h-10 w-10 p-2 mb-2"
+            className="bg-opacity-75 backdrop-filter backdrop-blur-md bg-white rounded-lg text-black h-10 w-10 p-2 mb-2"
             size={8}
           />
           <IoIosSettings
-            className="bg-opacity-80 bg-white rounded-lg text-black h-10 w-10 p-2 mb-2"
+            className="bg-opacity-75 backdrop-filter backdrop-blur-md bg-white rounded-lg text-black h-10 w-10 p-2 mb-2"
             size={8}
           />
           <FiPlusCircle
-            className="bg-opacity-80 bg-white rounded-lg text-black h-10 w-10 p-2"
+            className="bg-opacity-75 backdrop-filter backdrop-blur-md bg-white rounded-lg text-black h-10 w-10 p-2"
             size={8}
+            onClick={() => setPage('create')}
           />
         </div>
         <div className="flex z-10 h-10 ml-4 mt-4">
--- src/components/mainPage/index.ts
+++ src/components/mainPage/index.ts
@@ -1,3 +1,4 @@
 export { default as Post } from './post';
 export { default as Search } from './search';
+export { default as CreatePost } from './createPost';
 export { default as Map } from './map/dynamicMap';
