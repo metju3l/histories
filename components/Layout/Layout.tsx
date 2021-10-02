487e17e8ec23866d61d5a6be3356110f60c30c07
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-10-02 21:10:25 +0200

    icons

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -31,7 +31,28 @@ const Layout: React.FC<LayoutProps> = ({
       )}
       <Head>
         <title>{title}</title>
+        <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="keywords"
+          content="DELTA - Střední škola informatiky a ekonomie, s.r.o."
+        />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/icon-72x72.png"
+          rel="icon"
+          type="image/png"
+          sizes="72x72"
+        />
+        <link
+          href="/icons/icon-512x512.png"
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+        />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.jpg"></link>
+        <meta name="theme-color" content="#17A6FA" />
       </Head>
       {children}
     </div>
