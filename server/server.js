5f16bcb575a13bfc64593030ba0746aee8dc0fbe
krystofex <krystof.kratky2003@gmail.com>
2021-10-10 15:42:52 +0200

    express prepare

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/test', (req, res) => {
+    res.json(JSON.stringify({ ok: 'oke' }));
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log(`🚀 Server ready`);
+  });
+});
