c710744eb006bee142e67786d4c842671dfb01f6
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-10-15 18:40:49 +0200

    frontend - main page styles

--- components/PostCard/PostCard.tsx
+++ components/PostCard/PostCard.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 import React, { FC, useState } from 'react';
 import { useDeletePostMutation, usePostQuery } from '@graphql/post.graphql';
 import { useLikeMutation } from '@graphql/relations.graphql';
-import { BiShare, BiCollection } from 'react-icons/bi';
-import { FaMapMarkedAlt, FaRegComment } from 'react-icons/fa';
-import { HiOutlineHeart, HiOutlineLocationMarker } from 'react-icons/hi';
+import { FiSend } from 'react-icons/fi';
 import Image from 'next/image';
 import GeneratedProfileUrl from '@lib/functions/GeneratedProfileUrl';
-import { Button } from '@nextui-org/react';
+import { Avatar, Button, Grid, Modal } from '@nextui-org/react';
 import { toast } from 'react-hot-toast';
+import { MdPhotoCamera } from 'react-icons/md';
+import { AiFillLike, AiOutlineComment, AiOutlineMore } from 'react-icons/ai';
 
 const PostCard: FC<{
   isLoggedQuery: any;
@@ -19,8 +19,9 @@ const PostCard: FC<{
 
   const [deletePostMutation] = useDeletePostMutation();
   const [likeMutation] = useLikeMutation();
+  const [modal, setModal] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
   const [editMode, setEditMode] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
   if (loading) return <div>loading</div>;
   if (error) {
@@ -35,92 +36,144 @@ const PostCard: FC<{
     second: '2-digit',
   });
 
+  const postDate = new Date(data!.post.postDate).toLocaleDateString('cs-cz', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+
   return (
-    <div className="w-[75%] m-auto p-[1em] bg-white shadow-lg dark:bg-[#343233] rounded-xl text-text-light dark:text-white mb-8">
-      <div className="flex items-center">
-        <Link href={`/${data!.post.author.username}`}>
-          <a className="flex">
-            <div className="relative rounded-full w-12 h-12 mr-4">
-              <Image
-                src={GeneratedProfileUrl(
-                  data!.post.author.firstName,
-                  data!.post.author.lastName
-                )}
-                layout="fill"
-                objectFit="contain"
-                objectPosition="center"
-                className="rounded-full"
-                alt="Profile picture"
-              />
-            </div>
-            <a>
-              <div className="font-semibold text-lg">
-                {' '}
-                {data!.post.author.firstName} {data!.post.author.lastName}
-              </div>
-            </a>
-          </a>
-        </Link>
-      </div>
-      {isLoggedQuery?.data?.isLogged?.id === data!.post.author.id && (
-        <div
-          onClick={async (values) => {
-            setIsLoading(true);
-            try {
-              await deletePostMutation({
-                variables: { id },
-              });
-            } catch (error) {
-              // @ts-ignore
-              toast.error(error.message);
-            }
-            setIsLoading(false);
-          }}
-        >
-          {isLoading ? (
-            <Button loading loaderType="spinner" />
-          ) : (
-            <Button type="submit" color="error">
-              delete post
-            </Button>
-          )}
-        </div>
-      )}
-      {time}
-      <br />
-      {data?.post.description}
-      {data?.post.hashtags && (
-        <div>
-          hashtags:
-          {data!.post.hashtags.map((hashtag: any) => (
-            <Button key={hashtag.name} auto flat color="#ff4ecd">
-              {hashtag.name}
-            </Button>
-          ))}
-        </div>
-      )}
-      <div className="relative rounded-xl w-full mt-8">
-        {data!.post.url.map((url) => (
-          <div key={url} className="relative rounded-xl w-full h-[16em]">
-            <Image
-              src={url}
-              layout="fill"
-              objectFit="contain"
-              objectPosition="center"
-              className="rounded-xl"
-              alt="Profile picture"
+    <>
+      <Modal
+        aria-labelledby="modal-title"
+        open={modal}
+        onClose={() => setModal(false)}
+      >
+        {isLoggedQuery?.data?.isLogged?.id === data!.post.author.id ? (
+          <>
+            <ModalOption
+              onClick={async () => {
+                try {
+                  await deletePostMutation({
+                    variables: { id },
+                  });
+                } catch (error) {
+                  // @ts-ignore
+                  toast.error(error.message);
+                }
+              }}
+              text="Delete post"
+              warning
             />
+            <ModalOption onClick={() => setEditMode(true)} text="Edit" />
+          </>
+        ) : (
+          <>
+            <ModalOption
+              onClick={() => setModal(false)}
+              text="Report"
+              warning
+            />
+            <ModalOption
+              onClick={() => setModal(false)}
+              text="Unfollow"
+              warning
+            />
+          </>
+        )}
+        <ModalOption onClick={() => setModal(false)} text="Share" />
+        <ModalOption onClick={() => setModal(false)} text="Go to post" />
+        <ModalOption onClick={() => setModal(false)} text="Copy link" />
+        <ModalOption onClick={() => setModal(false)} text="Cancel" />
+      </Modal>
+
+      <div className="w-full m-auto bg-white dark:bg-[#343233] border-gray-[#DADBDA] border rounded-lg text-text-light dark:text-white mb-8">
+        <div className="w-full flex space-between p-[1em]">
+          <a className="w-full gap-[10px] h-18 flex items-center">
+            <Link href={`/${data!.post.author.username}`}>
+              <>
+                <Avatar
+                  size="medium"
+                  src={GeneratedProfileUrl(
+                    data!.post.author.firstName,
+                    data!.post.author.lastName
+                  )}
+                />
+                <div>
+                  <a className="font-semibold text-lg">
+                    {data!.post.author.firstName} {data!.post.author.lastName}
+                  </a>
+                  <a className="flex gap-[10px]">
+                    <MdPhotoCamera size={24} />
+                    {postDate}
+                  </a>
+                </div>
+              </>
+            </Link>
+          </a>
+          <button onClick={() => setModal(true)}>
+            <AiOutlineMore size={24} />
+          </button>
+        </div>
+
+        <div className="relative w-full">
+          <img src={data!.post.url[currentImage]} alt="Post" />
+          {currentImage > 0 && (
+            <button onClick={() => setCurrentImage(currentImage - 1)}>
+              {'<'}
+            </button>
+          )}
+          {currentImage < data!.post.url.length - 1 && (
+            <button onClick={() => setCurrentImage(currentImage + 1)}>
+              {'>'}
+            </button>
+          )}
+          <div className="flex gap-4">
+            <AiFillLike size={24} />
+            <AiOutlineComment size={24} />
+            <FiSend size={24} />
           </div>
-        ))}
-        <div className="absolute right-2 bottom-4 p-2 bg-white shadow-lg dark:bg-[#343233] rounded-xl text-text-light dark:text-white">
-          <Link href="/map">
-            <a>
-              <FaMapMarkedAlt size={24} />
-            </a>
-          </Link>
+          <div className="p-2">
+            {data?.post.description && (
+              <>
+                <a className="font-semibold">{data?.post.author.username}:</a>{' '}
+                {data?.post.description}
+              </>
+            )}
+
+            {data?.post.hashtags && data?.post.hashtags.length > 0 && (
+              <div>
+                <a className="font-semibold"> hashtags:</a>
+                {data!.post.hashtags.map((hashtag: any) => ` ${hashtag.name} `)}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
+  );
+};
+
+const ModalOption = ({
+  text,
+  onClick,
+  warning,
+}: {
+  text: string;
+  onClick: () => void;
+  warning?: boolean;
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${
+        warning ? 'text-red-500 font-semibold' : ''
+      } w-full border-b border-gray-[#DADBDA] py-4`}
+    >
+      {loading ? 'loading...' : text}
+    </button>
   );
 };
 
