5bca4392ae8e588ec4738424722fea10afd495d7
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-11-04 09:59:20 +0100

    sorting personalized posts by time

--- src/queries/PersonalizedPostsQuery.ts
+++ src/queries/PersonalizedPostsQuery.ts
@@ -1,14 +1,34 @@
 import RunCypherQuery from '../database/RunCypherQuery';
 
+// QUERY
+/*
+ * MATCH (user:User)                                          - match logged user
+ * WHERE ID(user) = 92
+ * CALL {
+ *    WITH user                                               - get user
+ *    OPTIONAL MATCH (author:User)-[:CREATED]->(post:Post)    - get posts created by followed users or by user
+ *    WHERE ((user)-[:FOLLOW]->(author)) OR (user = author)
+ *    RETURN post, author                                     - return posts and author
+ *    ORDER BY post.createdAt DESC                            - order posts by date
+ * }
+ * RETURN COLLECT(DISTINCT post{.*, id: ID(post)}) AS posts   - return posts as object
+ */
+
 const PersonalizedPostsQuery = async (logged: number | null) => {
   const query =
     logged === null
       ? `
       MATCH (post:Post)
       RETURN COLLECT(post{.*, id: ID(post)}) AS posts LIMIT 125`
-      : `
-      OPTIONAL MATCH (user:User), (author:User)-[:CREATED]->(post:Post)
-      WHERE ID(author) = ${logged} OR ((user)-[:FOLLOW]->(author:User) AND ID(user) = ${logged})
+      : `MATCH (user:User)
+      WHERE ID(user) = ${logged}
+      CALL {
+          WITH user
+          OPTIONAL MATCH (author:User)-[:CREATED]->(post:Post)
+          WHERE ((user)-[:FOLLOW]->(author)) OR (user = author)
+          RETURN post, author
+          ORDER BY post.createdAt DESC
+      }
       RETURN COLLECT(DISTINCT post{.*, id: ID(post)}) AS posts`;
 
   const result = await RunCypherQuery(query);
