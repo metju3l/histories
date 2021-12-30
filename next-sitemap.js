3c4ea11a95f19d86fc54089bf8853b63d43e00f5
Kryštof <krystof.kratky2003@gmail.com>
2021-12-30 16:23:06 +0100

    JWT in `.env` & build in gitpod init & sitemap confix

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,6 +3,9 @@ const siteUrl = 'https://www.histories.cc';
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   robotsTxtOptions: {
     policies: [
       { userAgent: '*', disallow: '/api/*' },
