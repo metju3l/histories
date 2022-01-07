cf3ad19ddd4718c3b21253d1664714346ca49686
krystofex <krystof.kratky2003@gmail.com>
2022-01-07 10:36:04 +0100

    deleted server-sitemap.xml

--- pages/server-sitemap.xml/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  return getServerSideProps(ctx);
-};
-
-const Site = () => {};
-
-export default Site;
