108810cb31a5bdae22f909bf8dfac2230b982659
Kryštof <krystof.kratky2003@gmail.com>
2022-01-09 09:52:23 +0000

    Delete old components

--- components/Modules/tmp/Comment/index.ts
+++
@@ -1,3 +0,0 @@
-export type { CommentProps } from './Comment';
-export { default as Comment } from './Comment';
-export { default as CommentHandler } from './CommentController';
--- components/Modules/tmp/Post/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as AddToCollectionModal } from './AddToCollectionModal';
-export { default as LikePost } from './LikePost';
-export { default as Post } from './Post';
-export type { default as PostProps } from './PostProps';
-export { default as PostTimeline } from './PostTimeline';
-export { default as UnlikePost } from './UnlikePost';
