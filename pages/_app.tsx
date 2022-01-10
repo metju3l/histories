f407d90fc60fcdfe7c79965c69046699a7c79cbc
Kryštof <krystof.kratky2003@gmail.com>
2022-01-10 08:50:45 +0000

    DELETED MS BLOATWARE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,21 +18,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ApolloProvider client={apolloClient}>
       <ThemeProvider attribute="class" defaultTheme="light">
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
-        />
-        <script
-          // this is here just because there is no better alternative and I don't agree with it
-          // I still don't like microsoft
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MS_CLARITY}");`,
-          }}
-        />
         <NextNprogress
           color={orange_main}
           height={2}
