bd1d05ed5c50cb2dc51d0aa9a5cb0042624cdd0d
Kryštof <krystof.kratky2003@gmail.com>
2022-01-17 10:47:53 +0000

    public playground fix

--- src/graphql/utils/apolloServer.ts
+++ src/graphql/utils/apolloServer.ts
@@ -6,6 +6,7 @@ import schema from './schema';
 
 const apolloServer = new ApolloServer({
   schema,
+    introspection: true, 
   plugins: [
     // graphql playground
     ApolloServerPluginLandingPageGraphQLPlayground({
