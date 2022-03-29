4b1c40e9613a444d481320e4fd4a143e59541acd
krystofex <krystof.kratky2003@gmail.com>
2022-03-29 21:13:35 +0200

    create post WIP

--- src/pages/create/post.tsx
+++ src/pages/create/post.tsx
@@ -1,4 +1,4 @@
-import { Input, Separator } from '@components/elements';
+import { Input } from '@components/elements';
 import Button from '@components/elements/buttons/Button';
 import { Layout } from '@components/layouts';
 import Search from '@components/modules/Search';
@@ -9,7 +9,6 @@ import {
   IsJwtValid,
   SSRRedirect,
 } from '@src/functions';
-import { motion } from 'framer-motion';
 import { GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
 import React, { useCallback, useEffect, useState } from 'react';
@@ -17,7 +16,7 @@ import { useDropzone } from 'react-dropzone';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
-import { HiOutlineChevronLeft } from 'react-icons/hi';
+import { HiLocationMarker } from 'react-icons/hi';
 import MapGL, {
   GeolocateControl,
   Marker,
@@ -235,104 +234,58 @@ const CreatePostPage: React.FC<CreatePostPageProps> = ({
         },
       }}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="w-full">
+      <div
+        className="w-full grid grid-cols-2"
+        style={{ height: 'calc(100vh - 56px)' }}
+      >
+        <div className="w-full h-full relative">
+          <MapGL
+            latitude={viewport.latitude}
+            longitude={viewport.longitude}
+            zoom={viewport.zoom}
+            width="100%"
+            height="100%"
+            mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            onViewportChange={setViewport}
+            dragRotate={false}
+          >
+            <GeolocateControl
+              style={{
+                bottom: 186,
+                right: 0,
+                padding: '10px',
+              }}
+              positionOptions={{ enableHighAccuracy: true }}
+            />
+            <Marker
+              longitude={viewport.longitude}
+              latitude={viewport.latitude}
+              offsetTop={-20}
+              offsetLeft={-10}
+            >
+              <HiLocationMarker className="w-10 h-10 text-brand -translate-x-1/2 -translate-y-full" />
+            </Marker>
+            <NavigationControl
+              style={{
+                bottom: 240,
+                right: 0,
+                padding: '10px',
+              }}
+              showCompass={false}
+            />
+          </MapGL>
+          {/* LOCATION SELECT */}
+          <div className="absolute w-64 top-2 left-2">
+            <Search setSearchCoordinates={setSearchCoordinates} />
+          </div>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          {/* PAGE HEADING */}
+          <h1 className="pb-1 text-3xl font-bold tracking-tight">
+            {t('Create post')}
+          </h1>
           <div className="w-full max-w-4xl px-6 m-auto xl:px-0">
-            <div className="flex pt-6 pb-8">
-              <div className="py-2">
-                {/* PAGE HEADING */}
-                <h1 className="pb-1 text-5xl font-bold tracking-tight">
-                  {t('Create post')}
-                </h1>
-              </div>
-            </div>
-            <Separator />
-            {/* LOCATION SELECT */}
-            <div className="flex w-full">
-              <div className="flex flex-col p-2 gap-2">
-                <Search setSearchCoordinates={setSearchCoordinates} />
-              </div>
-              <div className="w-full p-2">
-                <motion.div
-                  className="flex flex-col w-full p-2 text-gray-500 bg-white border border-gray-300 rounded-xl dark:border-gray-600 dark:text-gray-400 gap-2"
-                  animate={{ height: collapsed ? 'auto' : '40vh' }}
-                  transition={{
-                    duration: 0.2,
-                    ease: 'easeInOut',
-                  }}
-                >
-                  <div
-                    className="flex items-center cursor-pointer gap-1"
-                    onClick={() => setCollapsed(!collapsed)}
-                  >
-                    <motion.span
-                      animate={{ rotate: collapsed ? '270deg' : '90deg' }}
-                      transition={{
-                        duration: 0.2,
-                        ease: 'easeInOut',
-                      }}
-                    >
-                      <HiOutlineChevronLeft className="w-5 h-5 text-black" />
-                    </motion.span>
-                    <a className="font-medium text-black">Select on map</a>
-                  </div>
-                  {!collapsed && (
-                    <MapGL
-                      latitude={viewport.latitude}
-                      longitude={viewport.longitude}
-                      zoom={viewport.zoom}
-                      width="100%"
-                      height="100%"
-                      className="rounded-xl"
-                      mapboxApiAccessToken={
-                        process.env.NEXT_PUBLIC_MAPBOX_TOKEN
-                      }
-                      mapStyle="mapbox://styles/mapbox/streets-v11"
-                      onViewportChange={setViewport}
-                      dragRotate={false}
-                    >
-                      <GeolocateControl
-                        style={{
-                          bottom: 186,
-                          right: 0,
-                          padding: '10px',
-                        }}
-                        positionOptions={{ enableHighAccuracy: true }}
-                      />
-                      <Marker
-                        longitude={viewport.longitude}
-                        latitude={viewport.latitude}
-                        offsetTop={-20}
-                        offsetLeft={-10}
-                      >
-                        <svg
-                          height={20}
-                          viewBox="0 0 24 24"
-                          style={{
-                            fill: '#d00',
-                            stroke: 'none',
-                          }}
-                        >
-                          <path
-                            d="M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
-c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
-C20.1,15.8,20.2,15.8,20.2,15.7z"
-                          />
-                        </svg>
-                      </Marker>
-                      <NavigationControl
-                        style={{
-                          bottom: 240,
-                          right: 0,
-                          padding: '10px',
-                        }}
-                        showCompass={false}
-                      />
-                    </MapGL>
-                  )}
-                </motion.div>
-              </div>
-            </div>
             <div className="flex w-full">
               <div className="w-full p-2">
                 <div>
@@ -509,34 +462,34 @@ C20.1,15.8,20.2,15.8,20.2,15.7z"
               </div>
             </div>
           </div>
-        </div>
-        <div className="p-10 m-auto max-w-[27rem]">
-          <div className="mt-[60px]"></div>
-          <div className="flex w-full gap-2">
-            {tags.map((tag, index) => {
-              return (
-                <div
-                  className="p-2 text-white bg-indigo-500 rounded-xl"
-                  key={index}
-                >
-                  #{tag}
-                  <button
-                    className="ml-4 text-red-300"
-                    onClick={() => {
-                      const newTags = tags.filter((x) => x !== tag);
-                      setTags(newTags);
-                    }}
+          <div className="p-10 m-auto max-w-[27rem]">
+            <div className="mt-[60px]"></div>
+            <div className="flex w-full gap-2">
+              {tags.map((tag, index) => {
+                return (
+                  <div
+                    className="p-2 text-white bg-indigo-500 rounded-xl"
+                    key={index}
                   >
-                    x
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+                    #{tag}
+                    <button
+                      className="ml-4 text-red-300"
+                      onClick={() => {
+                        const newTags = tags.filter((x) => x !== tag);
+                        setTags(newTags);
+                      }}
+                    >
+                      x
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
 
-          <Button loading={isLoading}>submit</Button>
-        </div>
-      </form>
+            <Button loading={isLoading}>submit</Button>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 };
